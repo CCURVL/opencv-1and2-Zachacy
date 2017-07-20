af3d9639b3af7499238071a986f0ccc1b1b89c4f
Zachacy <tpre31738@gmail.com>
2017-07-20 20:17:05 +0800

    Zachacy hw2

--- Opencv_hw1and2/Opencvhw1_2/Source.cpp
+++ Opencv_hw1and2/Opencvhw1_2/Source.cpp
@@ -29,7 +29,7 @@ void mean_filter(Mat &image) {
 
 int HW01_TestFunction() {
 	// Fill your answer here
-	Mat image = imread("C:/Users/USER/Desktop/opencv-1and2-Zachacy/testImage/lena.jpg", 3);
+	Mat image = imread("lena.jpg", 3);
 	Mat Gray_image;
 	Mat Binary_image;
 	cvtColor(image, Gray_image, CV_BGR2GRAY);
@@ -50,17 +50,22 @@ int HW02_ReadAndShowData(int numberOfImgs) {
 	Mat Binary_image;
 	string name;
 	while (numberOfImgs > 0) {
-		name = "C:/Users/USER/Desktop/opencv-1and2-Zachacy/testImage/capture/" + to_string(9-numberOfImgs) + ".jpg";
+		name = to_string(9-numberOfImgs) + ".jpg";
 		image = imread(name, 3);
 		cvtColor(image, Gray_image, COLOR_RGB2GRAY);
-		threshold(Gray_image, Binary_image, 124,255, THRESH_BINARY);
-		imshow("Display_RGB", image);
-		imshow("Display_Gray", Gray_image);
-		imshow("Display_Binary", Binary_image);
+		threshold(Gray_image, Binary_image, 124, 255, THRESH_BINARY);
 		moveWindow("Display_RGB", 50, 50);
 		moveWindow("Display_Gray", 550, 50);
-		moveWindow("Display_Binary",1050, 50);
-		waitKey(0);
+		moveWindow("Display_Binary", 1050, 50);
+		while (true) {
+			if (waitKey(0) == 13)
+				break;
+			else {
+				imshow("Display_RGB", image);
+				imshow("Display_Gray", Gray_image);
+				imshow("Display_Binary", Binary_image);
+			}
+		}
 		destroyAllWindows();
 		numberOfImgs--;
 	}
